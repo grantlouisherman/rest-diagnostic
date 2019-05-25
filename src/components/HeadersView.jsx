0b83d1913bc30337a8b126df18649d268454cc03
Grant Herman <grantlouisherman041@gmail.com>
2019-05-25 10:22:45 -0700

    updating header view

--- src/components/HeadersView.jsx
+++ src/components/HeadersView.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const HeadersView = headers => {
     return (
+        <table>
+        <tr>
+          <th>Key</th>
+          <th>Value</th>
+        </tr>
+        <tr>
         {
-
+            headers ? headers.map(header => {
+                <label></label>
+                <input></input>
+            }) : null
         }
+        </tr>
+        </table>
+       
     )
 }
 
