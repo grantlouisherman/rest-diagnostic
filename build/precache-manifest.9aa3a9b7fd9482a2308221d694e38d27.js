b60c6a24eb7b43ffbe8374adadda02f6d00e97f3
Grant Herman <grantlouisherman041@gmail.com>
2019-06-06 19:44:34 -0700

    Adding build to see if I can use at work to test

---
+++ build/precache-manifest.9aa3a9b7fd9482a2308221d694e38d27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b88911cc6554fa20a34718f98bf4f74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c7f8b3abad4ab6a1442",
+    "url": "/static/css/main.af20723e.chunk.css"
+  },
+  {
+    "revision": "41e1d541bb00f7daa5e8",
+    "url": "/static/js/2.0057aca5.chunk.js"
+  },
+  {
+    "revision": "5c7f8b3abad4ab6a1442",
+    "url": "/static/js/main.e3ae3866.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
