2e4384776a777586495c9e97b43b4a81816fe328
grantlouisherman <grantlouisherman041@gmail.com>
2020-01-18 14:12:30 -0800

    Reverted changes of TS and added response code to icon

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Icon = ({ responseStaus}) => {
-    if(responseStaus === 200) {
-        return <i className="check icon"></i>
+    if( responseStaus >= 200 &&  responseStaus < 400) {
+        return <i className="check icon">{`Response Code: ${responseStaus}`}</i>
     }
 
-    if(responseStaus == 500){
-        return <i className="exclamation circle icon"></i>
+    if(responseStaus >= 400){
+        return <i className="exclamation circle icon">{`Response Code: ${responseStaus}`}</i>
     }
     return <i className="question circle icon"></i>
 }
